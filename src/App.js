6203558ddda9564eb8625b10bbed90f445a6eb74
Brian Oliver II <brian.d.oliver@icloud.com>
2021-04-21 07:19:19 -0400

    fixed geoLocation and date returning undefined bug

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import "bootstrap/dist/css/bootstrap.css";
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import View from "./components/View";
 
 function App() {
-  const [location, setLocation] = useState({})
-  const [date, setDate] = useState("")
+  const location = useRef()
+  const date = useRef()
   const [nasaImage, setNasaImage] = useState("");
   const [weatherData, setWeatherData] = useState({});
 
   const onSuccess = (position) => {
     const currDate = new Date().toISOString().slice(0,10);
-    setDate(currDate);
+    date.current = currDate; // current date
 
-    console.log(position)
+    console.log("GeoLocation Before toFixed", position)
     const currCoords = {
       latitude: new Number(position.coords.latitude).toFixed(2),
       longitude: new Number(position.coords.longitude).toFixed(2)
     }
-    setLocation(currCoords);
-    fetchWeatherData(location);
-    fetchNasaImage(location);
+    location.current = currCoords; // current geo location
+    console.log("GeoLocation After toFixed", location)
 
-    console.log(nasaImage)
+    fetchWeatherData(location);
+    fetchNasaImage(location, date);
   };
 
   const onError = (error) => {
     console.log(`GeoLocation Error: ${error}`)
-    setWeatherData({});
-    setNasaImage("");
   };
 
   useEffect(() => {
@@ -36,26 +34,31 @@ function App() {
   
  
   
-    const fetchNasaImage = async ({latitude, longitude}) => {
-      const query = `${process.env.REACT_APP_NASA_EARTH_API_URL}?api_key=${process.env.REACT_APP_NASA_API_KEY}&lat=${latitude}&lon=${longitude}&date=${date}`
-      
+  const fetchNasaImage = async (location, date) => {
+    try {
+      const query = `${process.env.REACT_APP_NASA_EARTH_API_URL}?api_key=${process.env.REACT_APP_NASA_API_KEY}&lat=${location.current.latitude}&lon=${location.current.longitude}&date=${date.current}`
+      console.log("NASA Fetch URL:", query)
       const response = await fetch(query);
       const nasaImage = await response.json();
       
       setNasaImage(nasaImage);
-      };
-  
-    console.log("Fetch Image")
+    } catch (err) {
+      console.log("ERR: NASA Image Error", err)
+    }
+  };
   
 
-  const fetchWeatherData = async ({latitude, longitude}) => {
-    const query = `${process.env.REACT_APP_OPEN_WEATHER_API_URL}?lat=${latitude}&lon=${longitude}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`;
-    
-    const response = await fetch(query);
-    const weatherData = await response.json();
-    
-    setWeatherData(weatherData);
-    };
+  const fetchWeatherData = async (location) => {
+    try {
+      const query = `${process.env.REACT_APP_OPEN_WEATHER_API_URL}?lat=${location.current.latitude}&lon=${location.current.longitude}&appid=${process.env.REACT_APP_OPEN_WEATHER_API_KEY}`;
+      console.log("Weather Fetch URL:", query)
+      const response = await fetch(query);
+      const weatherData = await response.json();
+      setWeatherData(weatherData);
+    } catch (err) {
+      console.log("ERR: Weather Error", err)
+    }
+  };
 
   return (
     <div>
