9e2c561a232ca2d2f6ee9d4e5378841a4bc0bc3b
BrianToombs <79666028+BrianToombs@users.noreply.github.com>
2021-04-20 22:05:33 -0400

    Revert "Toombs"

--- src/App.js
+++ src/App.js
@@ -1,52 +1,23 @@
 import "bootstrap/dist/css/bootstrap.css";
 import {useState, useEffect} from "react";
-import View from "./components/View";
 
 function App() {
-  const [location, setLocation] = useState({})
-  const [date, setDate] = useState("")
-  const [nasaImage, setNasaImage] = useState("");
-  const [weatherData, setWeatherData] = useState({});
-
-  const onSuccess = (position) => {
-    const currDate = new Date().toISOString().slice(0,10);
-    setDate(currDate);
-
-    console.log(position)
-    const currCoords = {
-      latitude: new Number(position.coords.latitude).toFixed(2),
-      longitude: new Number(position.coords.longitude).toFixed(2)
-    }
-    setLocation(currCoords);
-    fetchWeatherData(location);
-    fetchNasaImage(location);
-
-    console.log(nasaImage)
-  };
-
-  const onError = (error) => {
-    console.log(`GeoLocation Error: ${error}`)
-    setWeatherData({});
-    setNasaImage("");
-  };
+  const [longitude, setLongitude] = useState();
+  const [latitude, setLatitude] = useState();
 
   useEffect(() => {
-    return window.navigator.geolocation.getCurrentPosition(onSuccess, onError);
-  }, []);
-  
-  const fetchNasaImage = async ({latitude, longitude}) => {
-   console.log("Fetch Image")
-  };
-
-  const fetchWeatherData = async ({latitude, longitude}) => {
-    console.log("Fetch Weather")
-  };
+    const geoLocationData = (geoData) => {
+      setLatitude(geoData.coords.latitude);
+      setLongitude(geoData.coords.longitude)
+    };
+    window.navigator.geolocation.getCurrentPosition(geoLocationData);
+  }, [])
 
   return (
-    <div>
-      <View nasaImage={nasaImage} weatherData={weatherData}/>
+    <div className="App">
+      
     </div>
-  )
+  );
 }
 
 export default App;
